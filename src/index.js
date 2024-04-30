7913fdf234a925f2dd6bda6924776c870dd1afd7
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-04-30 15:12:07 +0530

    Wire Up : Redux Store with react component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import App from './App';
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux';
+import store from './store/Slices/configureStore';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
-    <App />
+<Provider store={store}>
+    <App/>
+</Provider>
 );
