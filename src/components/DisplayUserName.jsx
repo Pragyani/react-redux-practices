62ba05bed34b0fc1a41a508ebcddd8847e91ef78
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-05-02 12:56:38 +0530

    adding: styled component

--- src/components/DisplayUserName.jsx
+++ src/components/DisplayUserName.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 
 const DisplayUserName=()=>{
     return(
@@ -6,6 +7,26 @@ const DisplayUserName=()=>{
       </>
     )
 }
+const Wrapper = styled.section`
+  li{
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 1rem;
+    border-bottom: 1px solid #eee;
+    
+    &:first-child{
+      border-top: 1px solid #eee;
+    }
+  }
+  
+  .btn-delete{
+    background-color: transparent;
+    border: 0;
+    color: #fff;
+  }
+`
 
 
 export default DisplayUserName;
