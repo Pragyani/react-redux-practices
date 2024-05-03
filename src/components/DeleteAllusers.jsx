d74db15ee6c4b73c8cebd19bc53d328fa486f204
PRAGYA PANDEY <pragyani2004@gmail.com>
2024-05-03 13:31:15 +0530

    Add: style into an component

--- src/components/DeleteAllusers.jsx
+++ src/components/DeleteAllusers.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { deleteUsers } from "../store/Slices/userSlice";
 import { useDispatch } from "react-redux";
+import styled from "styled-components";
+
+
 const DeleteAllusers=()=>{
 
     const dispatch= useDispatch()
@@ -15,5 +18,19 @@ const DeleteAllusers=()=>{
     )
 }
 
+const Wrapper = styled.section`
+  .clear-btn{
+    text-transform: capitalize;
+    background: linear-gradient(135deg, #7f6de3 0%, #11aaf1 100%);
+    margin-top:2rem;
+    border-radius :3px;
+    padding: 10px;
+    letter-spacing:0.1px;
+    font-size:large;   
+    cursor: pointer;
+    color: #fff;
+    border: none;
+  }
+`
 
 export default DeleteAllusers
